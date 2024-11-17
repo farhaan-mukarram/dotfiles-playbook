7f3bece7f120f53ceb94df52cc57fd198ed5f54d
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-17 16:10:53 +0500

    fix: fix neovim install errors

--- config/build-neovim.sh
+++ config/build-neovim.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/neovim/neovim
-cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git clone https://github.com/neovim/neovim ~/neovim
+cd ~/neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
