c6fb65aa8213dd59b3ab511d2063019b19133c75
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-17 17:31:21 +0500

    fix: fix neovim build script not cloning repo

--- config/build-neovim.sh
+++ config/build-neovim.sh
@@ -1,2 +1,4 @@
-git clone https://github.com/neovim/neovim ~/neovim
-cd ~/neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git clone https://github.com/neovim/neovim ~/neovim -q
+cd ~/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
