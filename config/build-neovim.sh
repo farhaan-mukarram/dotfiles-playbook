839b5cede8d2b17a5034e876ad31ce946c4e277b
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-18 00:19:33 +0500

    refactor: remove redundant scripts

--- config/build-neovim.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/neovim/neovim ~/neovim -q
-cd ~/neovim
-git checkout stable
-make CMAKE_BUILD_TYPE=RelWithDebInfo
--- config/install-neovim.sh
+++
@@ -1,5 +0,0 @@
-cd ~/neovim
-rm -r build/  # clear the CMake cache
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
-make install
-export PATH="$HOME/neovim/bin:$PATH"
