c5c717b576a4e75998aeb3814eb4d75ae3f9ec06
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-17 15:31:06 +0500

    feat: add neovim installation steps to playbook

---
+++ config/build-neovim.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
---
+++ config/install-neovim.sh
@@ -0,0 +1,5 @@
+cd ~/neovim
+rm -r build/  # clear the CMake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+export PATH="$HOME/neovim/bin:$PATH"
