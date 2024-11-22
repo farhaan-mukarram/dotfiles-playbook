ae0857abf6340bc4890e163cb0dc4ed399974dd2
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-23 01:54:32 +0500

    fix: fix bootstrap script errors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 # Install ansible
 sudo apt update && sudo apt install software-properties-common && sudo add-apt-repository --yes --update ppa:ansible/ansible && sudo apt install ansible
 
+# Clone the repo
+git clone https://github.com/farhaan-mukarram/dotfiles-playbook.git ~
+
 # Run the playbook
-ansible-playbook --ask-become-pass main.yaml
+ansible-playbook --ask-become-pass ~/dotfiles-playbook/main.yaml
